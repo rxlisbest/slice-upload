d3633f0ab69368dacd5a82b756027a3a4a38fb0f
Roy <rxlisbest@gmail.com>
2019-07-11 14:35:39 +0800

    test

---
+++ tests/Qiniu/RequestTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Rxlisbest\SliceUpload\Tests\Qiniu;
+
+use PHPUnit\Framework\TestCase;
+
+class RequestTest extends TestCase
+{
+    public function testSetChunk()
+    {
+        $this->assertEquals(1, 1);
+    }
+}
---
+++ tests/SliceUploadTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Rxlisbest\SliceUpload\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Rxlisbest\PhpDate\PhpDate;
+use Rxlisbest\SliceUpload\SliceUpload;
+
+class SliceUploadTest extends TestCase
+{
+    public function testIndex()
+    {
+        $this->assertEquals(1, 1);
+    }
+}

7c4919c1b4c536b6ec176af7fa6411470884f924
Roy <rxlisbest@163.com>
2020-06-12 16:00:25 +0800

    test

--- tests/SliceUploadTest.php
+++ tests/SliceUploadTest.php
@@ -5,33 +5,23 @@ namespace Rxlisbest\SliceUpload\Tests;
 use PHPUnit\Framework\TestCase;
 use Rxlisbest\PhpDate\PhpDate;
 use Rxlisbest\SliceUpload\Request\WebUploaderRequest;
+use Rxlisbest\SliceUpload\RequestFactory;
 use Rxlisbest\SliceUpload\SliceUpload;
 use Rxlisbest\SliceUpload\Storage;
 
 class SliceUploadTest extends TestCase
 {
-    
     public function testSave()
     {
-        $mock = \Mockery::mock('overload:\Rxlisbest\SliceUpload\Storage');
-        $mock->allows('save')->once()->andReturn('success');
+        $mock = \Mockery::mock('overload:' . Storage::class);
         $mock->allows('setKey')->andReturn($mock);
-        $mock->allows('setChunk')->andReturn($mock);
-        $mock->allows('setName')->andReturn($mock);
-        $mock->allows('setChunks')->andReturn($mock);
-        $mock->allows('setTempDir')->andReturn($mock);
-        $mock->allows('setStream')->andReturn($mock);
+        $mock->allows('setRequest')->andReturn($mock);
+        $mock->allows('save')->once()->andReturn('success');
 
-        $requestMock = \Mockery::mock('\Rxlisbest\SliceUpload\Request\WebUploaderRequest');
-        $requestMock->allows('getChunk')->once()->andReturn(0);
-        $requestMock->allows('getChunks')->once()->andReturn(1);
-        $requestMock->allows('getKey')->once()->andReturn('');
-        $requestMock->allows('getName')->once()->andReturn('');
-        $requestMock->allows('getTempDir')->once()->andReturn('');
-        $requestMock->allows('getStream')->once()->andReturn('');
+        $requestMock = \Mockery::mock(WebUploaderRequest::class);
 
-        $mock = \Mockery::mock('overload:\Rxlisbest\SliceUpload\RequestFactory');
-        $mock->allows('create')->andReturn($requestMock);
+        $mock = \Mockery::mock('alias:' . RequestFactory::class);
+        $mock->shouldReceive('create')->andReturn($requestMock);
 
         $sliceUpload = new SliceUpload('/');
         $this->assertEquals('success', $sliceUpload->save(''));
